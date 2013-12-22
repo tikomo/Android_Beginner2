d53191e9cf6fe2eca1e20985925fc7c5b84e2754
tikomo <tikomo@gmail.com>
2013-12-22 23:07:44 +0900

    add readme.txt

--- test2/src/com/example/test2/MainActivity.java
+++ test2/src/com/example/test2/MainActivity.java
@@ -1,8 +1,17 @@
+/*
+ * ボタンのクリックイベント
+ * ランダム
+ * 
+ */
 package com.example.test2;
+
+import java.util.Random;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,9 +19,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    
     }
 
-
+    /*
+     * ボタンをクリックするたびに0,1,2のどれかの数値を表示します
+     * 
+     * 
+     */
+    public void button1_onClick(View view) {
+    	
+    	TextView textView1 = (TextView)findViewById(R.id.textView1);
+    	
+    	Random rnd = new Random();
+        int a = rnd.nextInt(3); // 0,1,2の値をとります
+        
+        textView1.setText(String.valueOf(a));
+    	
+    }
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
