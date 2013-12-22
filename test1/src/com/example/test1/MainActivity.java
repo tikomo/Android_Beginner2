aaac0cb4c40ec7d5ebe172d7cf9aa45787229e2b
tikomo <tikomo@gmail.com>
2013-12-22 23:07:28 +0900

    add readme.txt

--- test1/src/com/example/test1/MainActivity.java
+++ test1/src/com/example/test1/MainActivity.java
@@ -1,9 +1,15 @@
+/*
+ * テキストの表示
+ * 直接指定する方法とstrings.xmlから持ってくる方法
+ * 
+ */
 package com.example.test1;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -12,8 +18,17 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // dummy
-       Log.d("MyApp", "Hello World");
+        // 直接記述
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("Hello World2");
+        
+        // 文字列リソースから取得する場合
+        String str = getString(R.string.hello_world_a);  
+        String str2 = (String) getText(R.string.hello_world_a); 
+        textView.setText(str);
+       
+        
+        Log.d("MyApp", str2); 
     }
 
 
